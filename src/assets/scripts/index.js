ad0049ce89a8ce2e8d5e07d9834f234bc27f3481
Robert Higdon <robert@ulastnight.com>
2022-07-24 13:51:04 -0700

    Adding mobile styling

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,1 +1,20 @@
-console.log('Hello world');
+import Glide from '@glidejs/glide';
+
+const mobileNavLinks = document.querySelectorAll('.mobile-nav a');
+const mobileCheckbox = document.getElementById('mobileCheckbox');
+
+new Glide('.glide', {
+  startAt: 1,
+  peek: 100,
+  breakpoints: {
+    600: {
+      peek: 20
+    }
+  }
+}).mount();
+
+const handleLinkClick = e => {
+  mobileCheckbox.checked = false;
+};
+
+mobileNavLinks.forEach(link => link.addEventListener('click', handleLinkClick));
