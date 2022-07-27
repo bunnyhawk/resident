2fc30a4369aa4c59957793ce6708d29587a62863
Robert Higdon <robert@ulastnight.com>
2022-07-26 22:21:38 -0700

    adjustments for mobile and fixing svgs

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -3,15 +3,7 @@ import Glide from '@glidejs/glide';
 const mobileNavLinks = document.querySelectorAll('.mobile-nav a');
 const mobileCheckbox = document.getElementById('mobileCheckbox');
 
-new Glide('.glide', {
-  startAt: 1,
-  peek: 100,
-  breakpoints: {
-    600: {
-      peek: 20
-    }
-  }
-}).mount();
+new Glide('.glide').mount();
 
 const handleLinkClick = e => {
   mobileCheckbox.checked = false;
--- utils/transforms.js
+++ utils/transforms.js
@@ -10,14 +10,14 @@ module.exports = {
   htmlmin: (content, outputPath) =>
     shouldTransformHTML(outputPath)
       ? htmlminifier.minify(content, {
-          html5: true,
-          removeComments: true,
-          collapseWhitespace: true,
-          collapseBooleanAttributes: true,
-          removeRedundantAttributes: true,
-          removeScriptTypeAttributes: true,
-          removeStyleLinkTypeAttributes: true
-        })
+        html5: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true
+      })
       : content,
 
   safelink: (content, outputPath) => {
